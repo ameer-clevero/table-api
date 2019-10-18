60dbddfa110b7a3955d1b25b82ae228f985dbabe
Ameer Muhammed <ameer@clevero.com>
2019-10-18 16:05:21 +0530

    InitCommit

--- api.js
+++ api.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const db = require("./db");
 app.get('/', async(req, res) => {
-    await db.sequelize.query("SELECT TABLE_NAME FROM `TABLES`", {
-            type: db.Sequelize.QueryTypes.SELECT
-        })
-        .then(result => {
-            return res.send(result);
-        });
+    const result = await db.sequelize.query("SELECT TABLE_NAME FROM `TABLES`", {
+        type: db.Sequelize.QueryTypes.SELECT
+    })
+    return res.send(result);
 });
 app.listen(8008);
